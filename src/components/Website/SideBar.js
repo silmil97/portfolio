03a499de22d611e0ab86d725bfda3817500cba4e
silmil97 <85782295+silmil97@users.noreply.github.com>
2022-05-21 10:13:21 +0200

    fixed scrollbar issue

--- src/components/Website/SideBar.js
+++ src/components/Website/SideBar.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './SideBar.css'
-import { slide as Menu } from 'react-burger-menu';
+import { stack as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom'
 import { useState } from 'react';
 
 export default props => {
     const [lang, setLang] = useState('en');
     return (
-        <Menu>
+        <Menu styles={{backgroundcolor:'green'}}>
             <Link className="menu-item" to="de/" onClick={() => setLang('de')}>DE</Link>
             <Link className="menu-item" to="en/" onClick={() => setLang('en')}>EN</Link>
             <Link className="menu-item" to={lang == 'de' ? "de/" : "en"}>Home</Link>
-            <Link className="menu-item" to={lang == 'de' ? "/de/projects" : "/en/projects"}>{lang == 'de' ? 'Projekte' : 'Projects'}</Link>
+            <Link className="menu-item" to={lang == 'de' ? "/de/projekte" : "/en/projekte"}>{lang == 'de' ? 'Projekte' : 'Projects'}</Link>
             <Link className="menu-item" to={lang == 'de' ? "/de/about" : "/en/about"}>About</Link>
-            <Link className="menu-item" to={lang == 'de' ? "/de/contact" : "/en/contact"}>{lang == 'de' ? 'Kontakt' : 'Contact'}</Link>
+            <Link className="menu-item" to={lang == 'de' ? "/de/kontakt" : "/en/kontakt"}>{lang == 'de' ? 'Kontakt' : 'Contact'}</Link>
         </Menu>
 
     );
